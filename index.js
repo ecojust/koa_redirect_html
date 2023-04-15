f5d733d54863879b9b5fcf3d6a40e05c7713aa8b
桔子桑 <chencheng@rayeye.cn>
2023-04-15 12:29:26 +0800

    clean code & colorconsole

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-//使用koa复写入口文件
 const Koa = require("koa");
 const Router = require("koa-router");
 const views = require("koa-views");
 const serve = require("koa-static");
+const colorconsole = require("@kenworks/colorconsole");
 
 const app = new Koa();
-const router = new Router();
 
+const router = new Router();
+//设置路由
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "主页",
+  });
+});
+
+router.get("/project1", async (ctx, next) => {
+  await ctx.render("dist/index", {
+    title: "项目1",
+  });
+});
+
+router.get("/project2", async (ctx, next) => {
+  await ctx.render("dist2/index", {
+    title: "项目2",
+  });
+});
+
+router.get("/project3", async (ctx, next) => {
+  await ctx.render("dist3/index", {
+    title: "项目3",
+  });
+});
+
+//配置静态文件目录
 app.use(serve(__dirname));
 app.use(serve(__dirname + "/views/"));
 
@@ -21,31 +47,10 @@ app.use(
 app.use(router.routes());
 
 app.listen(3000);
-
-//设置路由
-router.get("/", async (ctx, next) => {
-  await ctx.render("index", {
-    title: "主页",
-  });
-});
-
-//设置路由
-router.get("/project1", async (ctx, next) => {
-  await ctx.render("dist/index", {
-    title: "项目1",
-  });
-});
-
-//设置路由
-router.get("/project2", async (ctx, next) => {
-  await ctx.render("dist2/index", {
-    title: "项目2",
-  });
-});
-
-//设置路由
-router.get("/project3", async (ctx, next) => {
-  await ctx.render("dist3/index", {
-    title: "项目3",
-  });
-});
+colorconsole.log("请确保本地3000端口未被占用", "green", null, "blink");
+colorconsole.log(
+  "服务已启动，请在浏览器打开如下地址，或者ctrl点击跳转",
+  "green",
+  null
+);
+colorconsole.log("http://localhost:3000", "green", null);
